ca245d14a1882a7c145e11af927ff8f900a6f50c
Malik <malikouda@gmail.com>
2020-04-06 21:45:19 -0500

    Fixed camera bug when using portals

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -17,34 +17,21 @@ public class CameraFollow : MonoBehaviour {
     private float smoothLookVelocityX;
     private float smoothLookVelocityY;
     private bool lookAheadStopped;
+    private Bounds oldBounds;
+    private Bounds newBounds;
 
     private void Start() {
         focusArea = new FocusArea(target.GetComponent<Collider2D>().bounds, focusAreaSize);
+        oldBounds = target.GetComponent<Collider2D>().bounds;
     }
 
-    private void LateUpdate() {
+    private void Update() {
         focusArea.Update(target.GetComponent<Collider2D>().bounds);
-
 
         Vector2 focusPosition = focusArea.center + Vector2.up * verticalOffset;
 
-        if (focusArea.velocity.x != 0) {
-            lookAheadDirX = Mathf.Sign(focusArea.velocity.x);
-            if (Mathf.Sign(player.input.x) == Mathf.Sign(focusArea.velocity.x) && player.input.x != 0) {
-                lookAheadStopped = false;
-                targetLookAheadX = lookAheadDirX * lookAheadDstX;
-            } else {
-                if (!lookAheadStopped) {
-                    lookAheadStopped = true;
-                    targetLookAheadX = currentLookAheadX + (lookAheadDirX * lookAheadDstX - currentLookAheadX) / 4f;
-                }
-            }
-        }
-
-        currentLookAheadX = Mathf.SmoothDamp(currentLookAheadX, targetLookAheadX, ref smoothLookVelocityX, lookSmoothTimeX);
-
         focusPosition.y = Mathf.SmoothDamp(transform.position.y, focusPosition.y, ref smoothLookVelocityY, verticalSmoothTime);
-        focusPosition += Vector2.right * currentLookAheadX;
+        focusPosition.x = Mathf.SmoothDamp(transform.position.x, focusPosition.x + currentLookAheadX, ref smoothLookVelocityX, verticalSmoothTime);
         transform.position = (Vector3)focusPosition + Vector3.forward * -10;
     }
 
