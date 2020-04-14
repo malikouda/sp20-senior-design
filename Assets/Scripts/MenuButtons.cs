dac82a712dffc5c042da96467bb1ca50245f8627
JimArnett <jimarnett98@gmail.com>
2020-04-14 15:27:42 -0500

    Main menu WIP

---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtons : MonoBehaviour
+{
+
+    public GameObject OptionsMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            OptionsMenu.SetActive(!OptionsMenu.activeInHierarchy);
+        }
+    }
+       
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void StartButton()
+    {
+        GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().NextLevel();
+    }
+
+    public void OptionButton()
+    {
+        OptionsMenu.SetActive(!OptionsMenu.activeInHierarchy);
+    }
+}
