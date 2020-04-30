b67290173fa13cbb991af3321028e52005eec80d
Malik <malikouda@gmail.com>
2020-04-30 15:05:01 -0500

    text popup

---
+++ Assets/Scripts/TextPopup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TextPopup : MonoBehaviour {
+
+    public GameObject text;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            text.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            text.SetActive(false);
+        }
+    }
+}
